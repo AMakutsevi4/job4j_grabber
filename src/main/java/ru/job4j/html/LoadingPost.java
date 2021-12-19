c4a11606ffbec15bf66150a6957205ff94da0203
Alexandr <amakutsevich@bk.ru>
2021-12-19 21:11:04 +0900

    2.3. Загрузка деталей поста.

--- src/main/java/ru/job4j/html/LoadingPost.java
+++ src/main/java/ru/job4j/html/LoadingPost.java
@@ -6,20 +6,26 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class LoadingPost {
+
+    public static String parsePage(String page) throws IOException {
+        Document doc = Jsoup.connect(page).get();
+        Elements header = doc.select(".messageHeader");
+        String[] name = header.text().split("\\.");
+        Elements content = doc.select(".msgBody");
+        Element description = content.get(1);
+        Elements data = doc.select(".msgFooter");
+        String[] date = data.text().split(",");
+        String[] time = date[1].split(" ");
+        return "Вакансия: " + name[0] + "\n"
+                + "Описание: " + description.text() + "\n"
+                + "Дата публикации вакансии: " + date[0] + "," + time[1] + "\n"
+                + "Ссылка на вакансию: " + time[2];
+    }
+
     public static void main(String[] args) throws IOException {
         String page = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
-        Document doc = Jsoup.connect(page).get();
-        Elements row = doc.select(".msgBody");
-        Element date = row.get(1);
-        int maxLength = 150;
-        Pattern p = Pattern.compile("\\G\\s*(.{1," + maxLength + "})(?=\\s|$)", Pattern.DOTALL);
-        Matcher m = p.matcher(date.text());
-        while (m.find()) {
-            System.out.println(m.group(1));
-        }
+        System.out.println(parsePage(page));
     }
 }
