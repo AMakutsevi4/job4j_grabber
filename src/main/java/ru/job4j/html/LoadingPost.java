4d3b3ddfa3c8b260f5df441fcdb604ca117b8062
Alexandr <amakutsevich@bk.ru>
2021-12-20 23:32:11 +0900

    2.3. Загрузка деталей поста.

--- src/main/java/ru/job4j/html/LoadingPost.java
+++ src/main/java/ru/job4j/html/LoadingPost.java
@@ -2,26 +2,24 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 import ru.job4j.post.Post;
 
 import java.io.IOException;
 
 public class LoadingPost {
 
-    public static String parsePage(String page) throws IOException {
-        Document doc = Jsoup.connect(page).get();
-        String header = doc.select(".messageHeader").get(0).ownText();
-        String content = doc.select(".msgBody").get(1).text();
-        String valueData = doc.select(".msgFooter").get(0).text();
-        String[] data = valueData.split(" \\[");
+    public static Post parsePage(String page) throws IOException {
         Post post = new Post();
         post.setLink(page);
-
-        return "Вакансия: " + header + "\n"
-                + "Описание: " + content + "\n"
-                + "Дата публикации вакансии: " + data[0] + "\n"
-                + "Ссылка на вакансию: " + post.getLink();
-
+        Document doc = Jsoup.connect(page).get();
+        post.setTitle(doc.select(".messageHeader").get(0).ownText());
+        post.setDescription(doc.select(".msgBody").get(1).text());
+        String valueData = doc.select(".msgFooter").get(0).text();
+        String[] data = valueData.split(" \\[");
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        post.setCreated(parser.parse(data[0]));
+        return post;
     }
 
     public static void main(String[] args) throws IOException {
