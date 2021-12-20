9cf6f82b782fa09fbe0456d2ba0beb2a37268b53
Alexandr <amakutsevich@bk.ru>
2021-12-20 14:20:27 +0900

    2.3. Загрузка деталей поста.

--- src/main/java/ru/job4j/html/LoadingPost.java
+++ src/main/java/ru/job4j/html/LoadingPost.java
@@ -2,8 +2,7 @@ package ru.job4j.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import ru.job4j.post.Post;
 
 import java.io.IOException;
 
@@ -11,17 +10,18 @@ public class LoadingPost {
 
     public static String parsePage(String page) throws IOException {
         Document doc = Jsoup.connect(page).get();
-        Elements header = doc.select(".messageHeader");
-        String[] name = header.text().split("\\.");
-        Elements content = doc.select(".msgBody");
-        Element description = content.get(1);
-        Elements data = doc.select(".msgFooter");
-        String[] date = data.text().split(",");
-        String[] time = date[1].split(" ");
-        return "Вакансия: " + name[0] + "\n"
-                + "Описание: " + description.text() + "\n"
-                + "Дата публикации вакансии: " + date[0] + "," + time[1] + "\n"
-                + "Ссылка на вакансию: " + time[2];
+        String header = doc.select(".messageHeader").get(0).ownText();
+        String content = doc.select(".msgBody").get(1).text();
+        String valueData = doc.select(".msgFooter").get(0).text();
+        String[] data = valueData.split(" \\[");
+        Post post = new Post();
+        post.setLink(page);
+
+        return "Вакансия: " + header + "\n"
+                + "Описание: " + content + "\n"
+                + "Дата публикации вакансии: " + data[0] + "\n"
+                + "Ссылка на вакансию: " + post.getLink();
+
     }
 
     public static void main(String[] args) throws IOException {
